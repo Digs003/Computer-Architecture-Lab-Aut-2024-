683946715905acadfae0c1f18743d940476adec4
Ecolash <codeandcode0@gmail.com>
2024-08-01 03:33:31 +0530

    Changes on files

--- Assignment-3/A3-Q1.asm
+++ Assignment-3/A3-Q1.asm
@@ -1,3 +1,10 @@
+## GROUP-26 
+#---------------------------------------------------------------
+##  TUHIN MONDAL(22CS10087)
+##  DIGANTA MANDAL(22CS30062)
+#---------------------------------------------------------------
+
+
 .data
 array:      .space 40
 input_msg:  .asciiz "Enter 10 integers: \n"
@@ -7,10 +14,15 @@ heap_msg:   .asciiz "Max Heap: "
 .globl main
 
 main:
-    jal     get_array
-    la      $s1, array
-    j       build_heap
+    jal     get_array       # Function to take array input
+    la      $s1, array      # Starting address of array
+    jal     build_heap      # Call build Heap Function
+    li      $v0, 10
+    syscall
 
+#---------------------------------------------------------------
+# FUNCTION TO TAKE ARRAY INPUT
+#---------------------------------------------------------------
 get_array:
     add     $t0, $zero, $zero
     la      $a0, input_msg
@@ -31,6 +43,10 @@ get_array:
     end_loop:
         jr      $ra
 
+
+#---------------------------------------------------------------
+# FUNCTION TO BUILD HEAP FROM ARRAY
+#---------------------------------------------------------------
 build_heap:
     li     $t0, 4
 
@@ -42,24 +58,26 @@ build_heap:
         j       heap_loop
 
     end_build:
-        # jr      $ra
-        j       print_array
+        jal     print_array
+        jr      $ra
 
-
+#---------------------------------------------------------------
+# RECURSIVE FUNCTION HEAPIFY TO ORDER VALUES IN ARRAY
+#---------------------------------------------------------------
 heapify:
     addi    $sp, $sp, -4
     sw      $ra, 0($sp)
     move    $s0, $a0
     sll     $t1, $a0, 1
-    addi    $t1, $t1, 1       # Left child index
-    addi    $t2, $t1, 1       # Right child index
+    addi    $t1, $t1, 1       # Left child index = 2i + 1
+    addi    $t2, $t1, 1       # Right child index = 2i + 2
 
     check_left:
         li      $t3, 10
         bge     $t1, $t3, check_right
         sll     $t4, $s0, 2
         add     $t4, $t4, $s1
-        lw      $t5, 0($t4)       # Parent value
+        lw      $t5, 0($t4)       # Largest value
         sll     $t6, $t1, 2
         add     $t6, $t6, $s1
         lw      $t7, 0($t6)       # Left child value
@@ -70,7 +88,7 @@ heapify:
         bge     $t2, $t3, compare
         sll     $t8, $s0, 2
         add     $t8, $t8, $s1
-        lw      $t5, 0($t8)       # Parent value
+        lw      $t5, 0($t8)       # Largest value
         sll     $t9, $t2, 2
         add     $t9, $t9, $s1
         lw      $t7, 0($t9)       # Right child value
@@ -81,7 +99,7 @@ heapify:
         beq     $s0, $a0, heapify_return
         sll     $t4, $a0, 2
         add     $t4, $t4, $s1
-        lw      $t5, 0($t4)       # Parent value
+        lw      $t5, 0($t4)       # Largest value
         sll     $t6, $s0, 2
         add     $t6, $t6, $s1
         lw      $t7, 0($t6)       # Child value
@@ -95,6 +113,9 @@ heapify:
         addi    $sp, $sp, 4
         jr      $ra
 
+#---------------------------------------------------------------
+# FUNCTION TO PRINT MAX HEAP OUTPUT
+#---------------------------------------------------------------
 print_array:
     la      $a0, heap_msg
     li      $v0, 4
@@ -113,8 +134,6 @@ print_array:
         j       print_loop
 
     end_print:
-        j       exit
+        jr		$ra					
 
-    exit:
-        li      $v0, 10
-        syscall
+
